416fb3332076fd7f3147bc36b01370fe996f9dc1
khldy <20212022103@std.nisantasi.edu.tr>
2024-09-08 03:53:17 +0300

    first

---
+++ script.js
@@ -0,0 +1,62 @@
+const videoLinks = {
+    "Darb'sJourney@J1": "https://www.https://youtu.be/Abg-aODlP7E?feature=shared.com/watch?v=Fpksdschk-o",
+    "Darb'sJourney@J2": "https://www.kotobati.com/book/%D9%83%D8%AA%D8%A7%D8%A8-%D9%85%D9%82%D8%A7%D8%B5%D8%AF-%D8%A7%D9%84%D8%B9%D8%A8%D8%A7%D8%AF%D8%A7%D8%AA",
+    "Darb'sJourney@J3": "https://youtu.be/9FLH-ceHXd0?feature=shared",
+    "Darb'sJourney@J4": "https://youtu.be/Gr-gZx8A4qA?feature=shared",
+    "Darb'sJourney@J5": "https://youtu.be/HD_RRRv2s7g?feature=shared",
+    "live&dieforallah": "https://youtu.be/I89L7Zc9M4M?feature=shared",
+    "live&dieforallah1": "https://youtu.be/1Z2vK1V2p9g?feature=shared",
+    "live&dieforallah2": "https://youtu.be/WE_jmLSKgP4?feature=shared",
+    "live&dieforallah3": "https://omran.org/ar/%D9%81%D9%8A-%D9%81%D9%84%D8%B3%D9%81%D8%A9-%D8%A7%D9%84%D8%B9%D8%A8%D8%A7%D8%AF%D8%A7%D8%AA?language=en",
+    "live&dieforallah4": "https://makkahacademy.org/our-life-and-islamic-culture-0011/",
+};
+
+function checkpassword() {
+    const password = document.getElementById('password').value;
+    const errorMessage = document.getElementById('error-message');
+    const successMessage = document.getElementById('success-message');
+    
+    if (videoLinks[password]) {
+        document.querySelector('.container').style.display = 'none';
+        successMessage.style.display = 'block';
+        successMessage.setAttribute('data-url', videoLinks[password]);
+        const videoElement = document.getElementById('congrats-video');
+        videoElement.play(); // Auto-play the video
+
+        triggerConfetti();
+    } else {
+        errorMessage.textContent = "الباسورد مش صح جرب تاني";
+        errorMessage.style.display = "block";
+        document.getElementById('password').value = "";
+    }
+}
+
+function triggerConfetti() {
+    const confettiContainer = document.getElementById('confetti');
+
+    // Create confetti pieces
+    for (let i = 0; i < 100; i++) {
+        const confettiPiece = document.createElement('div');
+        confettiPiece.classList.add('confetti-piece');
+        
+        // Randomize position and delay
+        confettiPiece.style.left = `${Math.random() * 100}vw`;
+        confettiPiece.style.animationDelay = `${Math.random() * 2}s`;
+        
+        confettiContainer.appendChild(confettiPiece);
+    }
+
+    // Stop confetti after 3 seconds
+    setTimeout(() => {
+        while (confettiContainer.firstChild) {
+            confettiContainer.removeChild(confettiContainer.firstChild);
+        }
+    }, 6000); // 3000 milliseconds = 3 seconds
+}
+
+
+function redirect() {
+    const successMessage = document.getElementById('success-message');
+    const videoUrl = successMessage.getAttribute('data-url');
+    window.location.href = videoUrl;
+}
